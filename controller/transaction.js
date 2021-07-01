04fcba066fbc96b80e40e5a2f67bae294dad698c
Emmanuel Ani <mailsforanih@gmail.com>
2021-07-02 00:24:10 +0100

    ensure that only xml and json request headers are supported

--- controller/transaction.js
+++ controller/transaction.js
@@ -4,66 +4,74 @@ const Validator = new Allvalidator();
 class Transaction {
 	transaction = (req, res) => {
 		try {
-			let data = '';
-			req.on('data', (chunk) => {
-				data += chunk;
-			});
+			if (
+				req.headers['content-type'] == 'application/xml' ||
+				req.headers['content-type'] == 'application/json'
+			) {
+				let data = '';
+				req.on('data', (chunk) => {
+					data += chunk;
+				});
 
-			req.on('end', () => {
-				res.setHeader('Content-Type', 'application/json');
-				const { cardNumber, expireDate, cvv, email, phoneNumber } =
-					JSON.parse(data);
-				let numberFormat;
-				if (!cardNumber || !expireDate || !cvv || !email || !phoneNumber) {
-					res.statusCode = 400;
-					return res.end(
-						JSON.stringify({
-							valid: false,
-							error:
-								'{cardNumber, expireDate, cvv,  email, phoneNumber} are required'
-						})
-					);
-				}
+				req.on('end', () => {
+					res.setHeader('Content-Type', 'application/json');
+					const { cardNumber, expireDate, cvv, email, phoneNumber } =
+						JSON.parse(data);
+					let numberFormat;
+					if (!cardNumber || !expireDate || !cvv || !email || !phoneNumber) {
+						res.statusCode = 400;
+						return res.end(
+							JSON.stringify({
+								valid: false,
+								error:
+									'{cardNumber, expireDate, cvv,  email, phoneNumber} are required'
+							})
+						);
+					}
 
-				if (!Validator.cardCheck(cardNumber)) {
-					res.statusCode = 400;
-					return res.end(
-						JSON.stringify({ valid: false, error: 'Card is invalid' })
-					);
-				}
+					if (!Validator.cardCheck(cardNumber)) {
+						res.statusCode = 400;
+						return res.end(
+							JSON.stringify({ valid: false, error: 'Card is invalid' })
+						);
+					}
 
-				if (!Validator.checkCardExpiration(expireDate)) {
-					res.statusCode = 400;
-					return res.end(
-						JSON.stringify({ valid: false, error: 'Card has Expired' })
-					);
-				}
+					if (!Validator.checkCardExpiration(expireDate)) {
+						res.statusCode = 400;
+						return res.end(
+							JSON.stringify({ valid: false, error: 'Card has Expired' })
+						);
+					}
 
-				if (!Validator.validatsCvv(cvv)) {
-					res.statusCode = 400;
-					return res.end(
-						JSON.stringify({ valid: false, error: 'Invalid CVV' })
-					);
-				}
+					if (!Validator.validatsCvv(cvv)) {
+						res.statusCode = 400;
+						return res.end(
+							JSON.stringify({ valid: false, error: 'Invalid CVV' })
+						);
+					}
 
-				if (Validator.phoneFormatter(phoneNumber) === 'Invalid Number') {
-					res.statusCode = 400;
-					return res.end(
-						JSON.stringify({ valid: false, error: 'Invalid Number' })
-					);
-				}
+					if (Validator.phoneFormatter(phoneNumber) === 'Invalid Number') {
+						res.statusCode = 400;
+						return res.end(
+							JSON.stringify({ valid: false, error: 'Invalid Number' })
+						);
+					}
 
-				numberFormat = Validator.phoneFormatter(phoneNumber);
-				let cardType = Validator.checkCardType(cardNumber);
-				let card = {
-					valid: true,
-					cardType,
-					numberFormat,
-					message: 'card charged'
-				};
-				res.statusCode = 200;
-				res.end(JSON.stringify(card));
-			});
+					numberFormat = Validator.phoneFormatter(phoneNumber);
+					let cardType = Validator.checkCardType(cardNumber);
+					let card = {
+						valid: true,
+						cardType,
+						numberFormat,
+						message: 'card charged'
+					};
+					res.statusCode = 200;
+					res.end(JSON.stringify(card));
+				});
+			} else {
+				res.statusCode = 400;
+				return res.end('Only requests in XML and JSON headers are supported');
+			}
 		} catch (error) {
 			res.statusCode = 503;
 			return res.end(JSON.stringify({ valid: false, error: error.message }));
